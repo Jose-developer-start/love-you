9a05068bf57da04f5940226b0f8a7edf129ff842
José Deodanes <60304928+Jose-developer-start@users.noreply.github.com>
2022-04-03 18:15:42 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var esmeraldita = document.getElementById('esmeraldita');
+var respuesta = document.getElementById("respuesta");
+esmeraldita.addEventListener('mouseenter', (e)=>{
+    esmeraldita.textContent="¿Te quieres casar conmigo?"
+    respuesta.textContent="Si, acepto!"
+})
